55430f523217aca03ee8b7e1f5d151179e44e81b
Jacob <JacobJardineuni@gmail.com>
2022-10-14 10:06:24 +0100

    Add project files.

---
+++ DashboardV2/Pages/Hearing.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class HearingModel : PageModel
+{
+    private readonly ILogger<HearingModel> _logger;
+
+    public HearingModel(ILogger<HearingModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
---
+++ DashboardV2/Pages/Users.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class MembersModel : PageModel
+{
+    private readonly ILogger<MembersModel> _logger;
+
+    public MembersModel(ILogger<MembersModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
